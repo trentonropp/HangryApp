3c20a9fe75bd763253d2669ffa52bc477ffea4fe
Lauren Arfaras <larfaras96@gmail.com>
2018-01-20 10:47:22 -0500

    Main Storyboard

---
+++ HangryApp/NetworkMgr.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkMgr.swift
+//  HangryApp
+//
+//  Created by Lauren Arfaras on 1/20/18.
+//  Copyright © 2018 Trenton and Lauren. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkMgr {
+    
+}
