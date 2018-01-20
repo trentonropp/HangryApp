5fb81524f90eed6ae75316747c85b72df493a575
Trenton Ropp <tropp2741@ufl.edu>
2018-01-20 13:25:00 -0500

    Added animations

--- HangryApp/ViewController.swift
+++ HangryApp/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    
+    @IBOutlet weak var GradBack2: UIImageView!
+    @IBOutlet weak var Mic1: UIImageView!
+    @IBOutlet weak var NowListening: UIImageView!
+    @IBOutlet weak var ListeningHolder: UIView!
+    @IBOutlet weak var GradBack1: UIImageView!
+    @IBOutlet weak var PowerButton: UIButton!
+    @IBOutlet weak var ImFeeling: UIImageView!
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var Mic2: UIImageView!
+    @IBOutlet weak var Mic3: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func PowerButton(_ sender: Any) {
+        ListeningHolder.isHidden = false
+        GradBack1.isHidden = true
+        PowerButton.isHidden = true
+        ImFeeling.isHidden = true
+        Logo.isHidden = true
+        _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.sayHello), userInfo: nil, repeats: true)
+        }
+    
+    var i = 1;
+    @objc func sayHello()
+    {
+        if(i==1){
+        self.Mic1.isHidden = false
+        self.Mic2.isHidden = true
+        self.Mic3.isHidden = true
+        }
+            
+        else if (i==2){
+            self.Mic1.isHidden = true
+            self.Mic2.isHidden = false
+            self.Mic3.isHidden = true
+        }
+        else {
+            self.Mic1.isHidden = true
+            self.Mic2.isHidden = true
+            self.Mic3.isHidden = false
+        }
+        i = i + 1
+        if(i==4){
+            i=1
+        }
     }
+   
 
 
+    
+   
 }
-
